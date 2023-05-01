a8231f9daddc2688fb51b712f609f79e30f50435
Malcottan <maxtryasiy@gmail.com>
2023-05-01 10:36:18 +0300

    Zvezdochki tut

---
+++ Project1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void piramidka (int side);
+
+void piramidka (int side){
+    int c = 1;
+    int i;
+    for(i = side; i > 0; --i){
+        for(int o = i-1; o > 0; --o){
+            cout << "_";
+        }
+        for(int p = c; p > 0; --p){
+            if(p == 1){
+                cout << "*" << endl;
+            } else {
+                cout << "*";
+            }
+        }
+        c += 2;
+
+    }
+}
+int side;
+int main(){
+    cin >> side;
+    piramidka(side);
+    return 0;
+}
