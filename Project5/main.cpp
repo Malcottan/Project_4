920eb627a5397ab0a055aa8a387f82bbc97dce2e
Malcottan <maxtryasiy@gmail.com>
2023-05-11 12:23:41 +0300

    V1.0

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 void dice();
@@ -8,37 +10,37 @@ void dice_vote(int a, int b)
 {
     if (a == b)
     {
-        if (a == 0){
+        if (a == 1){
             cout << " _____" <<  " _____" << endl;
             cout << "|     |" << "|     |" <<  endl;
             cout << "|  *  |" << "|  *  |" << endl;
             cout << "|_____|" << "|_____|" << endl;
             cout << endl;
-        } if (a == 1){
-            cout << " _____" <<  " _____" << endl;
-            cout << "|*    |" << "|*    |" <<  endl;
-            cout << "|     |" << "|     |" << endl;
-            cout << "|____*|" << "|____*|" << endl;
-            cout << endl;
         } if (a == 2){
             cout << " _____" <<  " _____" << endl;
             cout << "|*    |" << "|*    |" <<  endl;
-            cout << "|  *  |" << "|  *  |" << endl;
+            cout << "|     |" << "|     |" << endl;
             cout << "|____*|" << "|____*|" << endl;
             cout << endl;
         } if (a == 3){
             cout << " _____" <<  " _____" << endl;
+            cout << "|*    |" << "|*    |" <<  endl;
+            cout << "|  *  |" << "|  *  |" << endl;
+            cout << "|____*|" << "|____*|" << endl;
+            cout << endl;
+        } if (a == 4){
+            cout << " _____" <<  " _____" << endl;
             cout << "|*   *|" << "|*   *|" <<  endl;
             cout << "|     |" << "|     |" << endl;
             cout << "|*___*|" << "|*___*|" << endl;
             cout << endl;
-        } if (a == 4){
+        } if (a == 5){
             cout << " _____" <<  " _____" << endl;
             cout << "|*   *|" << "|*   *|" <<  endl;
             cout << "|  *  |" << "|  *  |" << endl;
             cout << "|*___*|" << "|*___*|" << endl;
             cout << endl;
-        } if (a == 5){
+        } if (a == 6){
             cout << " _____" <<  " _____" << endl;
             cout << "|* * *|" << "|* * *|" <<  endl;
             cout << "|     |" << "|     |" << endl;
@@ -46,92 +48,141 @@ void dice_vote(int a, int b)
             cout << endl;
         }
     }
-    else if (a == 0 or b == 0)
-    {
-        cout << " _____" << endl;
-        cout << "|     |" << endl;
-        cout << "|  *  |" << endl;
-        cout << "|_____|" << endl;
-        cout << endl;
-    } if (a == 1 or b == 1)
-    {
-        cout << " _____" << endl;
-        cout << "|*    |" << endl;
-        cout << "|     |" << endl;
-        cout << "|____*|" << endl;
-        cout << endl;
-    } if (a == 2 or b == 2)
-    {
-        cout << " _____" << endl;
-        cout << "|*    |" << endl;
-        cout << "|  *  |" << endl;
-        cout << "|____*|" << endl;
-        cout << endl;
-    } if (a == 3 or b == 3)
-    {
-        cout << " _____" << endl;
-        cout << "|*   *|" << endl;
-        cout << "|     |" << endl;
-        cout << "|*___*|" << endl;
-        cout << endl;
-    } if (a == 4 or b == 4)
-    {
-        cout << " _____" << endl;
-        cout << "|*   *|" << endl;
-        cout << "|  *  |" << endl;
-        cout << "|*___*|" << endl;
-        cout << endl;
-    } if (a == 5 or b == 5)
-    {
-        cout << " _____" << endl;
-        cout << "|* * *|" << endl;
-        cout << "|     |" << endl;
-        cout << "|*_*_*|" << endl;
-        cout << endl;
-    } 
+    else {
+        if (a == 1 or b == 1)
+        {
+            cout << " _____" << endl;
+            cout << "|     |" << endl;
+            cout << "|  *  |" << endl;
+            cout << "|_____|" << endl;
+            cout << endl;
+        } if (a == 2 or b == 2)
+        {
+            cout << " _____" << endl;
+            cout << "|*    |" << endl;
+            cout << "|     |" << endl;
+            cout << "|____*|" << endl;
+            cout << endl;
+        } if (a == 3 or b == 3)
+        {
+            cout << " _____" << endl;
+            cout << "|*    |" << endl;
+            cout << "|  *  |" << endl;
+            cout << "|____*|" << endl;
+            cout << endl;
+        } if (a == 4 or b == 4)
+        {
+            cout << " _____" << endl;
+            cout << "|*   *|" << endl;
+            cout << "|     |" << endl;
+            cout << "|*___*|" << endl;
+            cout << endl;
+        } if (a == 5 or b == 5)
+        {
+            cout << " _____" << endl;
+            cout << "|*   *|" << endl;
+            cout << "|  *  |" << endl;
+            cout << "|*___*|" << endl;
+            cout << endl;
+        } if (a == 6 or b == 6)
+        {
+            cout << " _____" << endl;
+            cout << "|* * *|" << endl;
+            cout << "|     |" << endl;
+            cout << "|*_*_*|" << endl;
+            cout << endl;
+        } 
+    }
 }
 void dice()
 {
     int a = 0, b = 0, c = 0, comp = 0, hum = 0;
+    srand(time(nullptr)); // Сід (або зерно)
     if ((c = rand() % 1) == 0)
     {
         cout << "The first move of the computer" << endl;
         a = rand() % 6 + 1;
         b = rand() % 6 + 1;
-        comp = a + b;
+        dice_vote(a, b);
+        comp = comp + a + b;
+        cout << ".";
+        _sleep(300);
+        cout << ".";
+        _sleep(300);
+        cout << "." << endl;
+        _sleep(300);
+        cout << "Your move, hit enter";
+        cin.get(); // Press enter
         a = rand() % 6 + 1;
         b = rand() % 6 + 1;
-        hum = a + b;
+        dice_vote(a, b);
+        hum = hum + a + b;
+        cout << "Score: Copm = " << comp << " You = " << hum << endl;
     }
     if ((c = rand() % 1) == 1)
     {
-        cout << "The first move is yours" << endl;
+        cout << "The first move is yours, hit enter" << endl;
         a = rand() % 6 + 1;
         b = rand() % 6 + 1;
-        hum = a + b;
+        cin.get();
+        cout << ".";
+        _sleep(300);
+        cout << ".";
+        _sleep(300);
+        cout << "." << endl;
+        _sleep(300);
+        dice_vote(a, b);
+        hum = hum + a + b;
+        cout << "Now the computer throws the dice" << endl;
         a = rand() % 6 + 1;
         b = rand() % 6 + 1;
-        comp = a + b;
+        dice_vote(a, b);
+        comp = comp + a + b;
+        cout << "Score: Copm = " << comp << " You = " << hum << endl;
     }
     for (int i = 0; i < 5; i++)
     {
         if (c == 0)
         {
+            cout << "The first move of the computer" << endl;
             a = rand() % 6 + 1;
             b = rand() % 6 + 1;
+            dice_vote(a, b);
             comp = comp + a + b;
+            cout << ".";
+            _sleep(300);
+            cout << ".";
+            _sleep(300);
+            cout << "." << endl;
+            _sleep(300);
+            cout << "Your move, hit enter";
+            cin.get(); // Press enter
             a = rand() % 6 + 1;
             b = rand() % 6 + 1;
+            dice_vote(a, b);
             hum = hum + a + b;
+            cout << "Score: Copm = " << comp << " You = " << hum << endl;
         }
         else
         {
+            cout << "The first move is yours, hit enter" << endl;
             a = rand() % 6 + 1;
             b = rand() % 6 + 1;
+            cin.get();
+            cout << ".";
+            _sleep(300);
+            cout << ".";
+            _sleep(300);
+            cout << "." << endl;
+            _sleep(300);
+            dice_vote(a, b);
             hum = hum + a + b;
+            cout << "Now the computer throws the dice" << endl;
             a = rand() % 6 + 1;
             b = rand() % 6 + 1;
+            dice_vote(a, b);
             comp = comp + a + b;
+            cout << "Score: Copm = " << comp << " You = " << hum << endl;
         }
     }
     if (comp > hum)
@@ -139,9 +190,12 @@ void dice()
         cout << "You lost, but don't get upset, better luck next time!" << endl;
         cout << "Your scope is " << hum << ", the computer had a scope of " << comp << endl;
     }
-    else
+    else if(comp < hum)
     {
         cout << "You won, well done!" << endl;
+        cout << "Your scope is " << hum << ", the computer had a scope of " << comp << endl;
+    } else {
+        cout << "A draw? Nothing will be lucky the second time ;)" << endl;
         cout << "Your scope is " << hum << ", the computer had a scope of " << comp << endl;
     }
 }
